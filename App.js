a207003cac21e7483e32a9fa0d3cfe5bb1c3e7a4
Kess <76202613+Kieran-Huon@users.noreply.github.com>
2022-11-10 18:48:33 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./Button.js";
+import React, { useState } from "react";
+import Cart from "./Cart.js";
+import ProductList from "./ProductList.js";
+import Test from "./test.js";
+import Crud from "./TP2/Crud.js";
+
+
+
+function App() {
+  return <Crud />;
+
+}
+
+export default App;
